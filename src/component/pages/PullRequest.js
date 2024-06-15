874c8136ac9d9abff99b7e1b74fddb12ffccc860
GemGemMin <kkm32543@khu.ac.kr>
2024-06-15 18:31:09 +0900

    add move to create-workspace
    3

--- src/component/pages/PullRequest.js
+++ src/component/pages/PullRequest.js
@@ -87,7 +87,7 @@ const PullRequest = () => {
       <h1>{fileData.title}</h1>
       <p style={{marginTop:'10px'}}>커밋 메시지 : {fileData.message}</p>
       <p style={{marginTop:'10px'}}>작성자 : {fileData.writer}</p>
-      <a style={{marginTop:'10px'}} href={fileData.fileUrl}>파일 다운로드</a>
+      <button className='btn1234' style={{marginTop:'10px'}} href={fileData.fileUrl}>파일 다운로드</button>
       
       {fileData.comments.length > 0 && (
       <div>
@@ -106,18 +106,23 @@ const PullRequest = () => {
       {fileData.writer !== userInfo.email && (
         <div>
           <input
+          className='btn123'
             type="text"
             value={newComment}
             onChange={handleCommentChange}
             placeholder="메시지 남기기"
           />
-          <button onClick={() => handleApproval(true)}>수락</button>
-          <button onClick={() => handleApproval(false)}>거절</button>
-          <button onClick={handleSubmit}>전송</button>
+          <div className="button-group-horizontal">
+              <button className='btn1234' onClick={() => handleApproval(true)}>수락</button>
+              <button style={{marginLeft:'10px'}} className='btn1234' onClick={() => handleApproval(false)}>거절</button>
+            </div>
+            <button className='btn1234 submit-btn' onClick={handleSubmit}>전송</button>
+          
         </div>
       )}
+      <button className='back-page-btn' onClick={() => handlebackbtn()}>뒤로 가기</button> 
     </div>
-    <button className='back-page-btn' onClick={() => handlebackbtn()}>뒤로 가기</button> 
+    
     </div>
   );
 };
