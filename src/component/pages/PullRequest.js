a866d648d99e17d4a546826935c393d98542861a
GemGemMin <kkm32543@khu.ac.kr>
2024-06-18 19:35:59 +0900

    add download btn

--- src/component/pages/PullRequest.js
+++ src/component/pages/PullRequest.js
@@ -27,6 +27,27 @@ const PullRequest = () => {
       }
     };
 
+    const handleFileDownload = async () => {
+      try {
+        const response = await axios.get(`http://125.250.17.196:1234/api/files/${fileData.fileId}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/octet-stream',
+            'responseType': 'blob'
+          }
+        });
+    
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', fileData.fileName);
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
     const fetchUserInfo = async () => {
       try {
         const response = await axios.get('http://125.250.17.196:1234/api/my/info', {
@@ -87,7 +108,7 @@ const PullRequest = () => {
       <h1>{fileData.title}</h1>
       <p style={{marginTop:'10px'}}>커밋 메시지 : {fileData.message}</p>
       <p style={{marginTop:'10px'}}>작성자 : {fileData.writer}</p>
-      <button className='btn1234' style={{marginTop:'10px'}} href={fileData.fileUrl}>파일 다운로드</button>
+      <button className='btn1234' style={{marginTop:'10px'}} onClick={handleFileDownload}>파일 다운로드</button>
       
       {fileData.comments.length > 0 && (
       <div>
